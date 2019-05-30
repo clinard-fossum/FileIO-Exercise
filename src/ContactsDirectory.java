d7a18547a8f2d9304c7227adc863ba55ddd93519
Alexandra <fossum.alexandra@gmail.com>
2019-05-30 16:40:27 -0500

    fiddled with the menu

--- src/ContactsDirectory.java
+++ src/ContactsDirectory.java
@@ -13,15 +13,15 @@ public class ContactsDirectory {
         Scanner sc = new Scanner(System.in);
         String topBar = "\n Name | Phone Number\n " + "-------------------\n ";
         String AskAbout =
-                "+-----------------------------------+\n" +
-                        "| 1. View contacts.\n" +
-                        "| 2. Add a new contact.\n" +
-                        "| 3. Search a contact by name.\n" +
-                        "| 4. Delete an existing contact.\n" +
-                        "| 5. Exit.\n" +
-                        "| Enter an option (1, 2, 3, 4 or 5):\n" +
-                        "+------------------------------------+\n";
-
+                "+---------------------------------+\n" +
+                        " 1. View contacts.\n" +
+                        " 2. Add a new contact.\n" +
+                        " 3. Search a contact by name.\n" +
+                        " 4. Delete an existing contact.\n" +
+                        " 5. Exit.\n" +
+                        " Enter an option (1, 2, 3, 4 or 5):\n" +
+                        "+----------------------------------+\n" +
+                        " ";
 
         Path folder = Paths.get(dataDirectory);
         Path file = Paths.get(dataDirectory, "contactExercise.txt");
